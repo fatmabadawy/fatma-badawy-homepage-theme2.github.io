311a04a6efbdb1c46c86468f55b3ec18e7157559
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-04-30 17:55:08 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+function function1() {
+    alert("Added to cart");
+}
+
+function function2(){
+   
+    
+    alert("Account made successfully.");
+    
+}
+
+function show() {
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'flex'; 
+}
+function hide() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'; 
+}
+
+function signup() {
+    window.location.href="./form.html";
+}
+function home() {
+    window.location.href="";
+}
+function login() {
+    window.location.href="file:///C:/Users/ms318/OneDrive/Desktop/project%201/login.html";
+}
+function complains() {
+    window.location.href="";
+} 
+function feedback() {
+    window.location.href="./feedbacktheme1.html";
+}
+function theme1() {
+    window.location.href="./home3.html";
+}
+function theme2() {
+  window.location.href="./index.html";
+}
+
+
+let availableKeywords = [
+    'clothes',
+    'shoes',
+    'skin care',
+    'supermarket',
+    'electronics devices',
+    'electricity',
+    'medical devices',
+    'bags',
+  ];
+
+  const resultBox = document.querySelector(".result-box");
+  const inputBox = document.getElementById("input-box");
+
+  inputBox.addEventListener('keyup', function() { 
+    let results = [];
+    let input = inputBox.value.toLowerCase();
+    if (input.length) {
+      results = availableKeywords.filter((keyword) => {
+        return keyword.toLowerCase().includes(input);
+      });
+      console.log(results);
+    }
+    display(results);
+    if (!results.length) {
+      resultBox.innerHTML = '';
+    }
+  });
+
+  function display(results) {
+    const content = results.map((list) => {
+      return "<li onclick=selectinput(this)>"+list+"</li>"
+    });
+    resultBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+  }
+
+  function selectInput(list) { 
+    inputBox.value = list.textContent;
+    resultBox.innerHTML = '';
+  }
+
+  function goButton() {
+    let search = inputBox.value.toLowerCase();
+    if (search.includes('clothes'))
+    {
+      window.location.href = "";
+    } 
+    else if (search.includes('skin care'))
+    {
+      window.location.href = "";
+    }
+    else if (search.includes('shoes'))
+    {
+      window.location.href = "";
+    }
+    else if (search.includes('medical'))
+    {
+      window.location.href = "";
+    }
+    else if (search.includes('super market'))
+    {
+      window.location.href = "";
+    }
+    else if (search.includes('bags'))
+    {
+      window.location.href = "";
+    }
+    else if (search.includes('electronics'))
+    {
+      window.location.href = "";
+    }
+    else if (search.includes('electricity'))
+    {
+      window.location.href = "";
+    }
+    
+  };
