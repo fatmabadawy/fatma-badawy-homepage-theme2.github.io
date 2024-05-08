26edf04e1b0c32860c9064f212ea7c7369c2c89c
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-05-08 14:05:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,135 +1,132 @@
 function function1() {
-    alert("Added to cart");
+  alert("Added to cart");
 }
 
 function function2(){
-   
-    
-    alert("Account made successfully.");
-    
+ 
+  
+  alert("Account made successfully.");
+  
 }
 
 function show() {
-    const sidebar = document.querySelector('.sidebar') 
-    sidebar.style.display = 'flex'; 
+  const sidebar = document.querySelector('.sidebar') 
+  sidebar.style.display = 'flex'; 
 }
 function hide() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'; 
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'; 
 }
 function signup() {
-    window.location.href="./form2.html";
+  window.location.href="./form2.html";
 }
 function home() {
-    window.location.href="./index.html";
+  window.location.href="./index.html";
 }
 function login() {
-    window.location.href="./login 1.html";
+  window.location.href="./login 1.html";
 }
 function complains() {
-    window.location.href="";
+  window.location.href="";
 } 
 function feedback() {
-    window.location.href="./feedback theme1.html";
+  window.location.href="./feedback theme1.html";
 }
 function theme1() {
-    window.location.href="./index.html";
+  window.location.href="./index.html";
 }
 function theme2() {
-  window.location.href="./home3.html";
+window.location.href="./home3.html";
 }
 function signup2() {
-    window.location.href="./form.html";
+  window.location.href="./form.html";
 }
 function home2() {
-    window.location.href="./home3.html";
+  window.location.href="./home3.html";
 }
 function login2() {
-    window.location.href="./login 2.html";
+  window.location.href="./login 2.html";
 }
 function complains2() {
-    window.location.href="";
+  window.location.href="";
 } 
 function feedback2() {
-    window.location.href="./feedback theme2.html";
+  window.location.href="./feedback theme2.html";
 }
 
+let availablekeywords1 = [
+  'clothes',
+  'shoes',
+  'skin care',
+  'supermarket',
+  'electronics devices',
+  'electricity',
+  'medical devices',
+  'books',
+];
+const resultbox=document.querySelector(".result-box");
+const inputbox=document.getElementById("input-box");
 
-let availableKeywords = [
-    'clothes',
-    'shoes',
-    'skin care',
-    'supermarket',
-    'electronics devices',
-    'electricity',
-    'medical devices',
-    'bags',
-    'video games',
-  ];
-
-  const resultBox = document.querySelector(".result-box");
-  const inputBox = document.getElementById("input-box");
-
-  inputBox.addEventListener('keyup', function() { 
-    let results = [];
-    let input = inputBox.value.toLowerCase();
-    if (input.length) {
-      results = availableKeywords.filter((keyword) => {
-        return keyword.toLowerCase().includes(input);
+inputbox.onkeyup = function()
+{
+  let result=[];
+  let input=inputbox.value;
+  if(input.length){
+      result=availablekeywords1.filter((keyword)=>{
+          return   keyword.toLowerCase().includes(input.toLowerCase());
       });
-      console.log(results);
-    }
-    display(results);
-    if (!results.length) {
-      resultBox.innerHTML = '';
-    }
+      console.log(result);
+  }
+ display(result);
+  if(!result.length)
+  {
+      resultbox.innerHTML='';
+  }
+}
+function display(result){
+  const content = result.map((list)=>{
+      return "<li onclick=selectinput(this)>"+list+"</li>";
   });
+  resultbox.innerHTML="<ul>"+ content.join('') +"</ul>";
+}
 
-  function display(results) {
-    const content = results.map((list) => {
-      return "<li onclick=selectinput(this)>"+list+"</li>"
-    });
-    resultBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+function selectinput(list){
+  inputbox.value=list.innerHTML;
+  resultbox.innerHTML='';
+}
+function goButton() {
+  let search = inputBox.value.toLowerCase();
+  if (search.includes('clothes'))
+  {
+    window.location.href = "";
+  } 
+  else if (search.includes('skin care'))
+  {
+    window.location.href = "fatma%20ayman%20theme%201/fatma%20ayman%20.html";
   }
-
-  function selectInput(list) { 
-    inputBox.value = list.textContent;
-    resultBox.innerHTML = '';
+  else if (search.includes('shoes'))
+  {
+    window.location.href = "";
   }
-
-  function goButton() {
-    let search = inputBox.value.toLowerCase();
-    if (search.includes('clothes'))
-    {
-      window.location.href = "";
-    } 
-    else if (search.includes('skin care'))
-    {
-      window.location.href = "fatma%20ayman%20theme%201/fatma%20ayman%20.html";
-    }
-    else if (search.includes('shoes'))
-    {
-      window.location.href = "";
-    }
-    else if (search.includes('medical'))
-    {
-      window.location.href = "";
-    }
-    else if (search.includes('super market'))
-    {
-      window.location.href = "";
-    }
-    else if (search.includes('bags'))
-    {
-      window.location.href = "";
-    }
-    else if (search.includes('electronics'))
-    {
-      window.location.href = "";
-    }
-    else if (search.includes('electricity'))
-    {
-      window.location.href = "";
-    }
-    
-  };
+  else if (search.includes('medical'))
+  {
+    window.location.href = "";
+  }
+  else if (search.includes('super market'))
+  {
+    window.location.href = "";
+  }
+  else if (search.includes('bags'))
+  {
+    window.location.href = "";
+  }
+  else if (search.includes('electronics'))
+  {
+    window.location.href = "";
+  }
+  else if (search.includes('electricity'))
+  {
+    window.location.href = "";
+  }
+  
+};
